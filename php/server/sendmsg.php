0c6e91e8749faa4ddaf4ee3ac34e4c858c92b627
Alexandre Silva <xande1231231@hotmail.com>
2019-12-06 18:31:05 -0300

    Fix

--- php/server/sendmsg.php
+++ php/server/sendmsg.php
@@ -9,6 +9,24 @@ if(isset($_COOKIE['iduser'],$_COOKIE['cry'])){
     VALUES ('".$owner."', '".$id_server."', '".$mensagem."')";
     if (mysqli_query($conn, $sql)) {
         echo "sucess";
+        if($mensagem === "/clear"){
+            $result_usuario = "SELECT * FROM server WHERE id = '$id_server' and owner = '$owner' LIMIT 1";
+            $resultado_usuario = mysqli_query($conn, $result_usuario);
+            $resultado = mysqli_fetch_assoc($resultado_usuario);
+            $mysqli = new mysqli($sql_db_host, $sql_db_user, $sql_db_pass, $sql_db_name);
+            if(isset($resultado)){
+            $sql = "DELETE FROM mensagem_server WHERE id_server = '$id_server'";
+            if ($conn->query($sql) === TRUE) {
+                $sql = "INSERT INTO mensagem_server (id_user, id_server, mensagem)
+                VALUES ('".$owner."', '".$id_server."', 'As mensagems foram apagadas')";
+                if (mysqli_query($conn, $sql)) {
+
+                }
+            } else {
+                echo "Error deleting record: " . $conn->error;
+            }
+        }
+        }
     } else{
         echo "error";
     }
