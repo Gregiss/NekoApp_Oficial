5800790b212ac5677dcc93a52a3f68f8703852ad
Alexandre Silva <xande1231231@hotmail.com>
2019-12-07 19:09:42 -0300

    WIP

--- php/server/myProfile.php
+++ php/server/myProfile.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 if(isset($_COOKIE['iduser'],$_COOKIE['cry'])){
 $mysqli = new mysqli($sql_db_host, $sql_db_user, $sql_db_pass, $sql_db_name);
     $userId = $_COOKIE['iduser'];
-    if ($result = $mysqli->query("SELECT id, nome, admin, status FROM user where id = '$userId'")) {
+    if ($result = $mysqli->query("SELECT id, nome, admin, status,avatar FROM user where id = '$userId'")) {
         while($row = $result->fetch_array(MYSQLI_ASSOC)) {
         $myArray = $row;
         }
