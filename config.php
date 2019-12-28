a38691aec94f71cd1113e8cce0abe22037188ce0
Alexandre Silva <xande1231231@hotmail.com>
2019-12-28 15:49:45 -0300

    Fix

--- config.php
+++ config.php
@@ -24,5 +24,5 @@ $email = "nekoapp404@gmail.com";
 // Senha email for send email (GMAIL)
 $senhamail = "xande123";
 // Site URL
-$site_url = "http://localhost:3000"; // e.g (http://example.com)
+$site_url = "http://187.4.221.93:3000"; // e.g (http://example.com)
 ?>
